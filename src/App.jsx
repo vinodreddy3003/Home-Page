b6d8f9e686077a7b83d59e84de42252d71d7c8be
vinodreddy3003 <138984613+vinodreddy3003@users.noreply.github.com>
2024-09-02 10:54:35 +0000

    fix: Navbar path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 import OrderSummary from "./components/OrderSummary";
 import Courses from "./components/Courses";
 import Web from "./components/Web";
